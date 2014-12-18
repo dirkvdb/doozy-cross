8b2cf0267f7eea5fcb7072867879b482ce62970c
Dirk Vanden Boer <dirk.vdb@gmail.com>
2014-12-18 23:31:04 +0100

    use arch toolchain

--- cross/buildall.sh
+++ cross/buildall.sh
@@ -3,8 +3,8 @@ rm -rf local
 mkdir -p local/lib
 mkdir -p local/include
 
-#export PATH=/opt/x-tools6h/arm-unknown-linux-gnueabihf/bin/:${PATH}
-export POSTFIX=-4.9
+export PATH=/opt/x-tools6h/arm-unknown-linux-gnueabihf/bin/:${PATH}
+#export POSTFIX=-4.9
 export CFLAGS="-march=armv6j -mfpu=vfp -mfloat-abi=hard -marm -O3"
 export ARMARCH=armv6
 
@@ -12,8 +12,8 @@ export ARMARCH=armv6
 #export CFLAGS="-march=armv7-a -mfpu=vfpv3 -mfloat-abi=hard -O3"
 #export ARMARCH=armv7
 
-export CROSS=arm-linux-gnueabihf-
-#export CROSS=arm-unknown-linux-gnueabihf-
+#export CROSS=arm-linux-gnueabihf-
+export CROSS=arm-unknown-linux-gnueabihf-
 export CXXFLAGS="${CFLAGS} -std=c++11"
 export CC=${CROSS}gcc${POSTFIX}
 export CXX=${CROSS}g++${POSTFIX}
